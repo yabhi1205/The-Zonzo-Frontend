8b6751c31dc89885c311a5711db671827bc52190
Yabhi1205 <83153764+yabhi1205@users.noreply.github.com>
2024-02-02 11:14:00 +0000

    fix the bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./Components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
